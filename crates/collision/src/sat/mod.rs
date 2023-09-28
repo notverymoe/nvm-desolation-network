bed86d8f58e1d17570d8bd60244e9632e5cc24a3
Natalie Baker <natalie.baker@advancednavigation.com>
2023-09-28 16:07:03 +0800

    Add Vec2 as SATShape

--- crates/collision/src/sat/mod.rs
+++ crates/collision/src/sat/mod.rs
@@ -21,3 +21,28 @@ pub trait SATShape {
 
     fn with_offset(self, offset: Vec2) -> Self;
 }
+
+impl SATShape for Vec2 {
+    const CAN_SMEAR_PROJECTION: bool = true;
+
+    fn project_on_axis(&self, axis: Vec2) -> Projection {
+        Projection::new(axis.dot(*self))
+    }
+
+    fn get_points(&self, out_points: &mut Vec<Vec2>) {
+        out_points.push(*self)
+    }
+
+    fn get_axes(&self, out_axes: &mut Vec<Vec2>, out_cache: &mut Vec<Projection>) {
+        out_axes.extend_from_slice(&[Vec2::X, Vec2::Y]);
+        out_cache.extend_from_slice(&[Projection::new(self.x), Projection::new(self.y)])
+    }
+
+    fn get_axes_derived(&self, _other: &[Vec2], _out_axes: &mut Vec<Vec2>) {
+        // no derived axes to test
+    }
+
+    fn with_offset(self, offset: Vec2) -> Self {
+        self + offset
+    }
+}
