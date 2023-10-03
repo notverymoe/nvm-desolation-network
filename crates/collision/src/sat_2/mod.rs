df09520edca8b683c2d33114dcc866987a9dd7a2
Natalie Baker <technology@notvery.moe>
2023-10-03 19:43:05 +0800

    Inline flipped cases

--- crates/collision/src/sat_2/mod.rs
+++ crates/collision/src/sat_2/mod.rs
@@ -21,30 +21,32 @@ pub fn get_seperating_axis_candidates(a: Shape, b: Shape) -> ([Vec2; 2], usize) 
     // TODO OPT nearest_axis in some cases could use AABB information
     
     match (a, b) {
-        (Shape::Point(_),   Shape::Point(_)) => AXES_0,
+        (  Shape::Point(_),   Shape::Point(_)) => AXES_0,
+        (   Shape::Line(a),    Shape::Line(b)) => axes_2(a.direction.perp(), b.direction.perp()),
+        ( Shape::Circle(a),  Shape::Circle(b)) => axes_1(axis_between(a.origin, b.origin)),
+        (   Shape::Rect(_),    Shape::Rect(_)) => AXES_0,
+        (Shape::Capsule(a), Shape::Capsule(b)) => axes_2(nearest_axis(b.origin, &a), nearest_axis(b.get_top(), &a)),
+        (  Shape::Slope(a),   Shape::Slope(b)) => axes_2(a.direction.perp(), b.direction.perp()),
+
         (Shape::Point(_),    Shape::Line(b)) | (   Shape::Line(b), Shape::Point(_)) => axes_2(b.direction, b.direction.perp()),
         (Shape::Point(a),  Shape::Circle(b)) | ( Shape::Circle(b), Shape::Point(a)) => axes_1(axis_between(a, b.origin)),
         (Shape::Point(_),    Shape::Rect(_)) | (   Shape::Rect(_), Shape::Point(_)) => AXES_0,
         (Shape::Point(a), Shape::Capsule(b)) | (Shape::Capsule(b), Shape::Point(a)) => axes_2(axis_between(a, b.origin), axis_between(a, b.get_top())),
         (Shape::Point(_),   Shape::Slope(b)) | (  Shape::Slope(b), Shape::Point(_)) => axes_1(b.direction.perp()),
 
-        (Shape::Line(a),    Shape::Line(b)) => axes_2(a.direction.perp(), b.direction.perp()),
         (Shape::Line(a),  Shape::Circle(b)) | ( Shape::Circle(b), Shape::Line(a)) => axes_1(nearest_axis(b.origin, &a)),
         (Shape::Line(a),    Shape::Rect(_)) | (   Shape::Rect(_), Shape::Line(a)) => axes_1(a.direction.perp()),
         (Shape::Line(a), Shape::Capsule(b)) | (Shape::Capsule(b), Shape::Line(a)) => axes_2(nearest_axis(a.origin, &b), nearest_axis(a.end, &b)),
         (Shape::Line(a),   Shape::Slope(b)) | (  Shape::Slope(b), Shape::Line(a)) => axes_2(a.direction.perp(), b.direction.perp()),
 
-        (Shape::Circle(a),  Shape::Circle(b)) => axes_1(axis_between(a.origin, b.origin)),
         (Shape::Circle(a),    Shape::Rect(b)) | (   Shape::Rect(b), Shape::Circle(a)) => axes_1(nearest_axis(a.origin, &b)),
         (Shape::Circle(a), Shape::Capsule(b)) | (Shape::Capsule(b), Shape::Circle(a)) => axes_2(nearest_axis(b.origin, &a), nearest_axis(b.get_top(), &a)),
         (Shape::Circle(a),   Shape::Slope(b)) | (  Shape::Slope(b), Shape::Circle(a)) => axes_2(nearest_axis(a.origin, &b), b.direction.perp()),
 
-        (Shape::Rect(_),    Shape::Rect(_)) => AXES_0,
-        (Shape::Rect(a), Shape::Capsule(b)) => axes_2(nearest_axis(b.origin, &a), nearest_axis(b.get_top(), &a)),
-        (Shape::Rect(_),   Shape::Slope(b)) => axes_1(b.direction.perp()),
+        (Shape::Rect(a), Shape::Capsule(b)) | (Shape::Capsule(b), Shape::Rect(a)) => axes_2(nearest_axis(b.origin, &a), nearest_axis(b.get_top(), &a)),
+        (Shape::Rect(_),   Shape::Slope(b)) | (  Shape::Slope(b), Shape::Rect(_)) => axes_1(b.direction.perp()),
 
-        (Shape::Capsule(a), Shape::Capsule(b)) => axes_2(nearest_axis(b.origin, &a), nearest_axis(b.get_top(), &a)),
-        (Shape::Capsule(a),   Shape::Slope(b)) => {
+        (Shape::Capsule(a),   Shape::Slope(b)) | (Shape::Slope(b), Shape::Capsule(a)) => {
             // if nearest is above the base of the cylinder, then we can exclude it
             // since the cylinder hemisphere could only collide with points bellow it
             // we'll include the nearest to the top without checking in that case
@@ -56,10 +58,6 @@ pub fn get_seperating_axis_candidates(a: Shape, b: Shape) -> ([Vec2; 2], usize) 
                 axes_2(b.direction.perp(), nearest_axis(a.get_top(), &b))
             }
         }
-
-        (Shape::Slope(a), Shape::Slope(b)) => axes_2(a.direction.perp(), b.direction.perp()),
-
-        (a, b) => get_seperating_axis_candidates(b, a), // TODO OPT inline?
     }
 }
 
