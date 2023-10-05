5cc4b036ba75245547889e44297ffbb898d7c8ff
Natalie Baker <technology@notvery.moe>
2023-10-05 19:58:11 +0800

    Refactor

--- crates/collision/src/lib.rs
+++ crates/collision/src/lib.rs
@@ -2,8 +2,20 @@
 
 pub mod shape;
 
-mod sat;
-pub use sat::*;
+mod sweep;
+pub use sweep::*;
+
+mod candidate_axes;
+pub use candidate_axes::*;
+
+mod contact;
+pub use contact::*;
+
+mod projection;
+pub use projection::*;
+
+mod test;
+pub use test::*;
 
 mod util;
 pub use util::*;
