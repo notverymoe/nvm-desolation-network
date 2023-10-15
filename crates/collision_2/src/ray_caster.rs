9c96e957189413c2469628ddbd0ac09e1cd323fd
Natalie Baker <technology@notvery.moe>
2023-10-15 20:57:25 +0800

    Add ray vs ray and ray vs line

--- crates/collision_2/src/ray_caster.rs
+++ crates/collision_2/src/ray_caster.rs
@@ -87,16 +87,93 @@ impl RayCaster {
         }
     }
 
+    pub fn find_ray_intersection(&self, other_origin: Vec2, other_dir: Vec2) -> Option<[Projection; 2]> {
+        let inv_pdp = 1.0/self.direction.perp_dot(other_dir);
+        if inv_pdp != f32::INFINITY {  
+            Some([
+                Projection::new(      other_dir.perp_dot( self.origin - other_origin) * inv_pdp),
+                Projection::new(-self.direction.perp_dot(other_origin -  self.origin) * inv_pdp),
+            ])
+        } else {
+            None
+        }
+    }
+
+    pub fn find_line_intersection(&self, from: Vec2, to: Vec2) -> Option<Projection> {
+        let offset = to - from;
+        let len = offset.length();
+        let dir = offset/len;
+        self.find_bounded_ray_intersection(from, dir, len)
+    }
+
+    pub fn find_bounded_ray_intersection(&self, from: Vec2, dir: Vec2, len: f32) -> Option<Projection> {
+        self.find_ray_intersection(from, dir).and_then(|[r, p]| if p.min() >= 0.0 && p.min() <= len { Some(r) } else { None })
+    }
+
 }
 
 fn find_circle_intersection_at_origin(
     ray_dp: [f32; 2],
     radius: f32
-) -> Option<Projection> {
+) -> Option<Projection> { 
     if radius < ray_dp[1].abs() { return None; }
     let offset = radius*(1.0-(ray_dp[1]/radius).powi(2)).sqrt();
     Some(Projection([
         -offset - ray_dp[0], 
          offset - ray_dp[0]
     ]))
+}
+
+#[cfg(test)]
+mod test {
+    use bevy::prelude::Vec2;
+
+    use crate::{RayCaster, Projection};
+
+    use std::f32::consts as f32;
+
+    fn test_ray_on(a: &RayCaster, b: &RayCaster) -> Option<[Projection; 2]> {
+        a.find_ray_intersection(b.origin(), b.direction())
+    }
+
+    macro_rules! assert_ray_test {
+        ($a:expr, $b:expr, $c:expr, $d:expr) => { assert_eq!(test_ray_on($a, $b), Some([Projection::new($c), Projection::new($d)])); };
+        ($a:expr, $b:expr) => { assert_eq!(test_ray_on($a, $b), None); };
+    }
+
+    #[test]
+    fn test_rays() {
+        let ray_py_along_px = RayCaster::new(Vec2::Y,  Vec2::X);
+        let ray_py_along_nx = RayCaster::new(Vec2::Y, -Vec2::X);
+        let ray_px_along_py = RayCaster::new(Vec2::X,  Vec2::Y);
+        let ray_px_along_ny = RayCaster::new(Vec2::X, -Vec2::Y);
+        let ray_zero_on_p45 = RayCaster::new(Vec2::ZERO, Vec2::ONE.normalize());
+        let ray_zero_on_ang = RayCaster::new(Vec2::ZERO, Vec2::new(10.0, 1.0).normalize());
+
+        assert_ray_test!(&ray_py_along_px, &ray_px_along_py,  1.0,  1.0);
+        assert_ray_test!(&ray_py_along_px, &ray_px_along_ny,  1.0, -1.0);
+        assert_ray_test!(&ray_py_along_px, &ray_zero_on_p45,  1.0,  f32::SQRT_2);
+        assert_ray_test!(&ray_py_along_px, &ray_zero_on_ang, 10.0,  (1.0_f32 + 10.0*10.0).sqrt());
+
+        assert_ray_test!(&ray_py_along_nx, &ray_px_along_py,  -1.0,  1.0);
+        assert_ray_test!(&ray_py_along_nx, &ray_px_along_ny,  -1.0, -1.0);
+        assert_ray_test!(&ray_py_along_nx, &ray_zero_on_p45,  -1.0,  f32::SQRT_2);
+        assert_ray_test!(&ray_py_along_nx, &ray_zero_on_ang, -10.0,  (1.0_f32 + 10.0*10.0).sqrt());
+    }
+
+    #[test]
+    fn test_lines() {
+        let ray_under = RayCaster::new(Vec2::Y * -100.0, Vec2::X);
+        let ray_above = RayCaster::new(Vec2::Y *  100.0, Vec2::X);
+        let ray_in    = RayCaster::new(Vec2::Y, Vec2::X);
+
+        let line_org = Vec2::X;
+        let line_dir = Vec2::Y;
+        let line_len = 10.0;
+
+        assert_eq!(ray_under.find_bounded_ray_intersection(line_org, line_dir, line_len), None);
+        assert_eq!(   ray_in.find_bounded_ray_intersection(line_org, line_dir, line_len), Some(Projection::new(1.0)));
+        assert_eq!(ray_above.find_bounded_ray_intersection(line_org, line_dir, line_len), None);
+    }
+
 }
